ebec21c046ad3842774f2c1ec9d36b9015e64b78
wlhwtyyjtm <wlhwtyyjtm@qq.com>
2016-12-15 18:01:41 +0800

    创建获取数据接口
    接口使用dropwizard实现，采用get方式获取数据

---
+++ DataReceiverService/src/test/java/com/chance/data/receiver/service/resources/DataReceiverResourceTest.java
@@ -0,0 +1,15 @@
+package com.chance.data.receiver.service.resources;
+
+import junit.framework.TestCase;
+
+public class DataReceiverResourceTest extends TestCase {
+
+  public void testUploadData() {
+    boolean useTest = true;
+    if (useTest) {
+      System.out.println("============== Mothed : testUploadData ==================");
+
+    }
+  }
+  
+}
